2f5019c40b63b730fd903709fbf37e1098716242
Maximillien Bingabo <maximillien.bingabo@wiredin.rw>
2020-07-02 09:55:21 +0200

    [Layout] adding nifty layout - basic setup

--- frontend/views/layouts/login.php
+++ frontend/views/layouts/login.php
@@ -248,8 +248,7 @@ AppAsset::register($this);
 
                 <div class="pad-all text-center">
                     <h3>Welcome back to the Dashboard.</h3>
-                    <p>Scroll down to see quick links and overviews of your Server, To do list, Order status or get
-                        some Help using Nifty.</p>
+                    <p>Scroll down to see quick links and overviews of your Server, To do list, Order status or get some Help using Nifty.</p>
                 </div>
             </div>
 
