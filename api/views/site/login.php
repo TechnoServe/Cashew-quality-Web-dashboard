204c802a137be519cd23203869d295f4ea236045
Maximillien Bingabo <maximillien.bingabo@wiredin.rw>
2020-07-03 09:32:14 +0200

    [Layout] setup layout on backend , #5226

--- api/views/site/login.php
+++ api/views/site/login.php
@@ -11,17 +11,12 @@ $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-login">
-
-    <div class="panel-body">
-
-        <div class="pad-btm mar-btm">
-            <?= Html::img('@web/img/logo.png', ['alt' => 'TechnoServe Logo', 'class' => 'img-lg', 'style' => ['width' => 'auto']]) ?>
-        </div>
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p> <?= Yii::t("app", "Please fill out the following fields to login") ?></p>
 
-
+    <div class="row">
+        <div class="col-lg-5">
             <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
 
                 <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
@@ -51,6 +46,6 @@ $this->params['breadcrumbs'][] = $this->title;
                 'items' => $languageItems->toArray(),
             ]);
             ?>
-
+        </div>
     </div>
 </div>
