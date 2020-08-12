d6556addebfe2d8d0112008488ae84cb988e8aba
Guy <guy.kandekwe@rexvirt.com>
2020-08-12 12:20:43 +0200

    #5269 List&details_buyers

--- api/models/User.php
+++ api/models/User.php
@@ -4,5 +4,19 @@ namespace api\models;
 
 class User extends \common\models\User
 {
+public function fields()
 
+{
+        $fields = parent::fields();
+        unset(
+            $fields['password_hash'],
+            $fields['password_reset_token'],
+            $fields['auth_key'],
+            $fields['verification_token'],
+            $fields['updated_at'],
+            $fields['company_id'],
+            $fields['role']);
+
+        return $fields;
+    }
 }
