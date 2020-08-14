6d80df63964d7698aaf305ec641e7bf9a7c3ab21
Guy <guy.kandekwe@rexvirt.com>
2020-08-14 09:55:14 +0200

    #5269 List_details_of_specific_buyer

--- api/controllers/BuyerController.php
+++ api/controllers/BuyerController.php
@@ -2,15 +2,13 @@
 
 namespace api\controllers;
 
-use backend\models\Qar;
-use common\models\QarDetail;
-use common\models\User;
+use api\components\ApiError;
+use api\components\ApiResponse;
+use api\models\User;
 use Yii;
 use yii\filters\auth\HttpBasicAuth;
+use yii\filters\VerbFilter;
 use yii\rest\ActiveController;
-use yii\web\BadRequestHttpException;
-use yii\filters\auth\QueryParamAuth;
-use yii\web\Response;
 
 
 class BuyerController extends ActiveController
@@ -27,11 +25,21 @@ class BuyerController extends ActiveController
                 }
             }
         ];
-        return $behaviors;
+        return array_merge(
+            $behaviors,
+            [
+                'verbs' => [
+                    'class' => VerbFilter::className(),
+                    'actions' => [
+                        'index' => ['GET'],
+                        'view' => ['GET']
+                    ],
+                ],
+            ]);
     }
 
     public $modelClass = 'api\models\User';
-    // Some reserved attributes like maybe 'q' for searching all fields at once 
+    // Some reserved attributes like maybe 'q' for searching all fields at once
     // or 'sort' which is already supported by Yii RESTful API
    // public $reservedParams = ['sort','q'];
 
@@ -44,48 +52,58 @@ class BuyerController extends ActiveController
         return $actions;
     }
 
-    public function actionIndex() {
-        $response = Yii::$app->response;
-        $response->format = Response::FORMAT_JSON;
-        
-        $buyers = $this->modelClass::find()->where(['role' => $this->modelClass::ROLE_FIELD_BUYER])->all();
 
-        $data = [];
-        foreach ($buyers as $buyer) {
-            $data[] = $buyer;
-        }
-        if ($buyers) {
-            return $response->data = [
-                        'data' => $data,
-                        'code' => 200
-                    ];
-        } else {
-            return $response->data = [
-                        'data' => 'Not Found',
-                        'code' => 404
-                    ];
-        }
-        
+    /**
+     * Method can be passed parameters which will be used to filter result
+     * Responds to request to get list of fieldTechs
+     * @return array
+     */
+    public function actionIndex() {
+
+        // Initiate search query
+        $query = $this->modelClass::queryByCompany();
+
+        // Search has to be performed on active fieldTechs
+        $query->andWhere(["role" => User::ROLE_FIELD_BUYER]) ->andWhere(["status" => User::STATUS_ACTIVE]);
+
+        // Get filter parameters from query params
+        $filter =  Yii::$app->request->getQueryParams();
+
+        // Filter by username if passed
+        (isset($filter['username']) && $filter['username']) ? $query->andFilterWhere(['like', 'username', trim($filter['username'])]) : null;
+
+        // Filter by name if passed
+        (isset($filter['name']) && $filter['name']) ? $query->andFilterWhere(['or',
+                ['like', 'first_name', trim($filter['name'])],
+                ['like', 'middle_name', trim($filter['name'])],
+                ['like', 'last_name', trim($filter['name'])],
+            ]
+        ) : null;
+
+        // Filter by email if passed
+        (isset($filter['email']) && $filter['email']) ? $query->andFilterWhere(['like', 'email', trim($filter['email'])]) : null;
+
+        // Filter by phone if passed
+        (isset($filter['phone']) && $filter['phone'])? $query->andFilterWhere(['like', 'phone', trim($filter['phone'])]) : null;
+
+        return new ApiResponse($query->all(), null, true);
     }
 
-    public function actionView($id) {
-        $response = Yii::$app->response;
-        $response->format = Response::FORMAT_JSON;
-        
-            $buyer = $this->modelClass::findOne(['id' => $id, 'role' => $this->modelClass::ROLE_FIELD_BUYER]);
 
-            if($buyer){
-                return $response->data = [
-                        'data' => $buyer,
-                        'code' => 200
-                    ];
-            }else{
-                return $response->data = [
-                        'data' => 'Not Found',
-                        'code' => 404
-                    ];
-            
-            }
+    /**
+     * Handles request to get details of a specific fieldTech by id
+     * @param $id
+     * @return ApiResponse
+     */
+    public function actionView($id)
+    {
+        $field_tech = $this->modelClass::queryByCompany()->andWhere(['id' => $id, 'role' => $this->modelClass::ROLE_FIELD_BUYER])->one();
+
+        if ($field_tech) {
+            return new ApiResponse($field_tech, null, true);
+        } else {
+            Yii::$app->response->statusCode = 404;
+            return new ApiResponse(null, [new ApiError(ApiError::INVALID_DATA, "Invalid Buyer ID")], false);
         }
-
+    }
 }
