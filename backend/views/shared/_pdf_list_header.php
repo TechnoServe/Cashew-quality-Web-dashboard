6d5f1bffa24008a0145b13baafbbd29293d82267
Bonaparte ITUZE <bonaparte.ituze@wiredin.rw>
2020-08-21 10:15:32 +0200

    Mpdf image base url

--- backend/views/shared/_pdf_list_header.php
+++ backend/views/shared/_pdf_list_header.php
@@ -5,6 +5,6 @@ use yii\helpers\Html;
 ?>
 <!--Pdf heading-->
 <div style="width: 80%; margin: 0 auto; text-align: center;">
-    <?=Html::img('@web/img/logo.png', ['width'=>'150'])?>
+    <?=Html::img( Yii::$app->params[ 'MPDF_IMAGE_BASE_URL'] . '/img/logo.png', ['width'=>'150'])?>
     <h3 style="margin-top: 20px; text-transform: uppercase"><?=$title?></h3>
 </div>
