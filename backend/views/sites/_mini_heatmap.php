b1ac8fe2eab1ca34d8b1f64de888f07555257c75
Blandine <blandine.kaneza@gmail.com>
2020-09-02 15:30:37 +0200

    refs #5402 Add Map with marker clusters

--- backend/views/sites/_mini_heatmap.php
+++ backend/views/sites/_mini_heatmap.php
@@ -1,9 +1,13 @@
-<?php
-use yii\web\JqueryAsset;
-$this->registerJsFile("https://code.highcharts.com/mapdata/countries/".strtolower($country_code)."/" . strtolower($country_code) . "-all.js", ["depends"=>JqueryAsset::className()]) ?>
+<html>
 
-<script src="https://code.highcharts.com/maps/highmaps.js"></script>
-
+<head>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/proj4js/2.3.6/proj4.js"></script>
+    <script src="https://code.highcharts.com/maps/highmaps.js"></script>
+    <script src="https://code.highcharts.com/stock/modules/data.js"></script>
+    <script src="https://code.highcharts.com/mapdata/custom/europe.js"></script>
+    <script src="https://code.highcharts.com/modules/marker-clusters.js"></script>
+    <script src="https://code.highcharts.com/modules/coloraxis.js"></script>
+</head>
 <style>
     #map-container {
         height: 800px;
@@ -19,54 +23,92 @@ $this->registerJsFile("https://code.highcharts.com/mapdata/countries/".strtolowe
     }
 </style>
 
+<body>
     <div class="container">
         <div id="map-container"></div>
     </div>
-
-    <?php $this->registerJs("
-    
-      Highcharts.mapChart('map-container', {
-            chart: {
-                map: 'countries/" . strtolower($country_code) . "/".strtolower($country_code)."-all'
-            },
-
-            title: {
-                text: null
-            },
-
-            credits: {
-                enabled: false
-            },
-
-            // tooltip: {
-            //     headerFormat: '',
-            //     pointFormat: 'Site: <b>{point.name}</b>, Location: <b>{point.name}</b><br>Lat: <b>{point.properties.latitude}</b>, Lon: <b>{point.properties.longitude}</b><br>KOR: <b>{point.properties.hasc}</b>'
-            // },
-
-            mapNavigation: {
-                enabled: true,
-                buttonOptions: {
-                    verticalAlign: 'top'
-                }
-            },
-
-            colorAxis: {
-                min: 0
-            },
-
-            series: [{
-                data: " . $siteSeries . ",
-                name: 'Average of KOR',
-                states: {
-                    hover: {
-                        color: '#BADA55'
+    <script>
+        // Add series with state capital bubbles
+        Highcharts.getJSON('https://cdn.jsdelivr.net/gh/highcharts/highcharts@1e9e659c2d60fbe27ef0b41e2f93112dd68fb7a3/samples/data/european-train-stations-near-airports.json', function(data) {
+            Highcharts.mapChart('map-container', {
+                chart: {
+                    map: 'custom/europe'
+                },
+                title: {
+                    text: 'Highcharts Maps lat/lon demo'
+                },
+                mapNavigation: {
+                    enabled: true
+                },
+                tooltip: {
+                    headerFormat: '',
+                    pointFormat: '<b>{point.name}</b><br>Lat: {point.lat:.2f}, Lon: {point.lon:.2f}'
+                },
+                colorAxis: {
+                    min: 0,
+                    max: 20
+                },
+                plotOptions: {
+                    mappoint: {
+                        cluster: {
+                            enabled: true,
+                            allowOverlap: false,
+                            animation: {
+                                duration: 450
+                            },
+                            layoutAlgorithm: {
+                                type: 'grid',
+                                gridSize: 70
+                            },
+                            zones: [{
+                                from: 1,
+                                to: 4,
+                                marker: {
+                                    radius: 13
+                                }
+                            }, {
+                                from: 5,
+                                to: 9,
+                                marker: {
+                                    radius: 15
+                                }
+                            }, {
+                                from: 10,
+                                to: 15,
+                                marker: {
+                                    radius: 17
+                                }
+                            }, {
+                                from: 16,
+                                to: 20,
+                                marker: {
+                                    radius: 19
+                                }
+                            }, {
+                                from: 21,
+                                to: 100,
+                                marker: {
+                                    radius: 21
+                                }
+                            }]
+                        }
                     }
                 },
-                dataLabels: {
-                    enabled: true,
-                    format: '{point.name}'
-                }
-            }]
+                series: [{
+                    name: 'Basemap',
+                    borderColor: '#A0A0A0',
+                    nullColor: 'rgba(177, 244, 177, 0.5)',
+                    showInLegend: false
+                }, {
+                    type: 'mappoint',
+                    enableMouseTracking: true,
+                    colorKey: 'clusterPointsAmount',
+                    name: 'Cities',
+                    data: data
+                }]
+            });
         });
-    
-    ") ?>
+    </script>
+</body>
+
+</html>
