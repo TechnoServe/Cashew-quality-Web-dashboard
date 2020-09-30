da9004df018d37ef11f5876e5f10e266627fe58b
Bonaparte ITUZE <bonaparte.ituze@wiredin.rw>
2020-09-30 13:36:18 +0200

    refs #5518 add form submit when click reset

--- backend/web/js/form.js
+++ backend/web/js/form.js
@@ -2,5 +2,6 @@ $(function (){
     $(document).on("click", ".form-reset-button", function (){
         $(this).closest("form").find("select").val(null).trigger("change");
         $(this).closest("form").find("input[type=text]").val(null).trigger("change");
+        $(this).closest("form").submit();
     })
 })
