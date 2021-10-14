3a072607e8acf1e15cadf5f3f1a9e020f1505e11
Bonaparte ITUZE <bonaparte.ituze@wiredin.rw>
2021-10-14 18:22:46 +0200

    Remove twilio creds in the repo

--- api/config/params.php
+++ api/config/params.php
@@ -1,7 +1,4 @@
 <?php
 return [
     'adminEmail' => 'admin@example.com',
-    'twilioSid' => 'ACe62f213f7dce76e86dbf6fc6cc161b17',
-    'twilioToken' => '5da53c537c0c0c641f3cccc6a266d160',
-    'twilioServiceSid' => 'MGf31794e1e16cbdf171813b3e8ce21bc1'
 ];
